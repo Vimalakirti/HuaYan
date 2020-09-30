fc2c5ce3f16ed7a94458e23df9dcb2a26d3b7fac
Vimalakirti <r08942078@ntu.edu.tw>
2020-10-01 00:53:57 +0800

    fix count.py

--- count.py
+++ count.py
@@ -1,19 +1,12 @@
 """
 八十卷《大方廣佛華嚴經》有幾個字呢？
 """
-
+from utils import Lines_in_sutra
 count = 0
-def format_num(d):
-    if d//10 == 0:
-        return "0{}".format(d)
-    else:
-        return str(d)
-
-for j in range(1, 81):
-    with open('T0279/T0279_0{}.txt'.format(format_num(j))) as f:
-        readdata = f.readlines()
-        for i_line in range(9, len(readdata)):
-            line = readdata[i_line]
-            count+=len(line)-1 # 去掉換行符
+for volume in range(1, 81):
+    readdata = Lines_in_sutra(volume)
+    for row in range(9, len(readdata)):
+        line = readdata[row]
+        count+=len(line)-1 # 去掉換行符
 
 print("《大方廣佛華嚴經》總字數：{}字。".format(count))
